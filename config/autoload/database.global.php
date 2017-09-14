19603b5ebb6a4b87da0c825129ca48539c52c69f
gpdolotina <gpdolotina@gmail.com>
2017-09-14 06:04:11 +0000

    Initial commit

---
+++ config/autoload/database.global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'driver'   => 'Pdo',
+        'dsn'      => 'pgsql:host=localhost;port=5432;',
+        'database' => 'your-database-name',
+        'user'     => 'your-username',
+        'password' => 'your-password'
+    ],
+];
